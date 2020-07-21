b8b779614cf1d9f33f7ae1bfcc3b71bfd50bbada
John Traas <j.j.h.traas@gmail.com>
2020-07-21 15:31:51 +0000

    added all tests + ran tests successfully

--- todo/test_models.py
+++ todo/test_models.py
@@ -5,5 +5,9 @@ from .models import Item
 class TestModels(TestCase):
 
     def test_done_defaults_to_false(self):
-        item = Item.objects.create(name='Done false ya hear, done false')
+        item = Item.objects.create(name='Done false ya hear, done false!')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Mind you, return ya self ya hear?!')
+        self.assertEqual(str(item), 'Mind you, return ya self ya hear?!')
